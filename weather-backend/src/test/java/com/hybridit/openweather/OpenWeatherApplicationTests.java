5a2b852ba99bf761fa390f8d6ef97b02384f3ae2
Boba <bobaruljic@gmail.com>
2022-06-24 12:16:07 +0200

    spring boot project setup

---
+++ weather-backend/src/test/java/com/hybridit/openweather/OpenWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hybridit.openweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenWeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
