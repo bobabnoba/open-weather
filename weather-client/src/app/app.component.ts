7fdaaadb6316aa36c981552432492ef4847f0d21
Boba <bobaruljic@gmail.com>
2022-06-25 14:35:38 +0200

    design home page

--- weather-client/src/app/app.component.ts
+++ weather-client/src/app/app.component.ts
@@ -7,5 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'weather-client';
-  selectedCountryCode = "NL"
+  selectedCountryCode = "GB"
+  temp = "20"
+  daysWithTemp = [
+    { day: 'FRIDAY', temp: 22 },
+    { day: 'SATURDAY', temp: 25 },
+    { day: 'SUNDAY', temp: 25 },
+    { day: 'MONDAY', temp: 26 },
+    { day: 'TUESDAY', temp: 24 },
+  ]
 }
