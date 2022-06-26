00fc48f1db3ca803c0fa087480c455004bf05a88
Boba <bobaruljic@gmail.com>
2022-06-26 14:19:57 +0200

    add env config for frontend app

--- weather-client/src/app/services/city-weather.service.ts
+++ weather-client/src/app/services/city-weather.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { City } from '../interfaces/city';
 import { CityAvgTemp } from '../interfaces/city-avg-temp';
 
@@ -11,12 +12,15 @@ export class CityWeatherService {
 
   constructor(private _http : HttpClient) { }
 
+  private apiUrl = environment.apiBaseUrl;
+
+
   getAllCities() : Observable<City[]> {
-    return this._http.get<City[]>("http://localhost:8080" + "/cities")
+    return this._http.get<City[]>(`${this.apiUrl}/cities`)
   }
 
   getAvgTemp(cityIds : number[], from : string | null, to : string | null, sortType : String) : Observable<CityAvgTemp[]> {
-    return this._http.get<CityAvgTemp[]>("http://localhost:8080" + "/weather/avg-temperature"
+    return this._http.get<CityAvgTemp[]>(`${this.apiUrl}/weather/avg-temperature` 
     + "?cityIds=" + cityIds[0] + "&from=" + from + "&to=" + to + "&sortBy=" + sortType)
   }
 }
